7f3558f44d338c98f4fc4fc0027e889b13baf7d4
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-07-19 04:00:20 -0400

    Restructure to dynamically check files on access
    Replace ServeMux with a custom implementation that decides whether to
    hand off control to the executing file or a fileserver. This allows for
    adding new executable files without having to restart QuickServ or
    re-scan.
    
    Squashed commit of the following:
    
    commit dcba54744038e80cb43951845d2fcd94700095b7
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Mon Jul 19 03:59:00 2021 -0400
    
        Fix minor glitch
    
    commit 9302175b23f76395a08e774e4f82a4a95405ef3f
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Mon Jul 19 03:48:33 2021 -0400
    
        Minor fixes and edits
    
    commit c4a4e89e2ed85bbb6bf76eea828d48faab4e9204
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Mon Jul 19 03:35:01 2021 -0400
    
        Finish dynamic file execution implementation
    
    commit 5c1f91ad3e7d4ebb6c6c44215df19412f4a11dbd
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Mon Jul 19 01:22:21 2021 -0400
    
        Clean up dead code
    
    commit 7d37bf3c27f082ce433af322364fe56926be3d8c
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Mon Jul 19 00:13:39 2021 -0400
    
        Run executables in main handler
    
    commit acfc51e2186100db52a25913041e26139158e9d7
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Sun Jul 18 23:29:02 2021 -0400
    
        Add functionality to main handler and refactor
    
    commit 142a0486853091a88324f1ec5d0d471087a79aa7
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Sun Jul 18 17:22:23 2021 -0400
    
        Refactor
    
    commit 3982dd2ff65ef2b2de212df51a3cd6cfb8349ab4
    Author: Jacob Strieb <jstrieb@users.noreply.github.com>
    Date:   Sun Jul 18 17:09:26 2021 -0400
    
        Handle with fileserver by default

--- quickserv.go
+++ quickserv.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strconv"
@@ -25,7 +26,6 @@ import (
  *****************************************************************************/
 
 var logger *log.Logger
-var routes map[string]string
 
 /******************************************************************************
  * Helper Functions
@@ -44,7 +44,6 @@ func GetLocalIP() string {
 	defer conn.Close()
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
 	return localAddr.IP.String()
 }
 
@@ -89,178 +88,271 @@ func DecodeForm(form url.Values) ([]byte, error) {
 	return []byte(form_data), nil
 }
 
-// NewExecutableHandler returns a handler for an executable path that, when
-// accessed: executes the file at the path, passes the request body via
-// standard input, gets the response via standard output and returns that as
-// the response body. The returned function is a closure over the path.
-func NewExecutableHandler(path string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logger.Println("Executing:", path)
-
-		abspath, err := filepath.Abs(path)
-		if err != nil {
-			logger.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		dir, _ := filepath.Split(abspath)
-
-		// Create the command using all environment variables. Include a
-		// REQUEST_METHOD environment variable in imitation of CGI
-		cmd := exec.Command(abspath)
-		cmd.Env = append(os.Environ(), "REQUEST_METHOD="+r.Method)
-
-		// Execute the route in its own directory so relative paths behave
-		// sensibly
-		cmd.Dir = dir
-
-		// Pass headers as environment variables in imitation of CGI
-		for k, v := range r.Header {
-			// The same header can have multiple values
-			for _, s := range v {
-				cmd.Env = append(cmd.Env, "HTTP_"+strings.ReplaceAll(k, "-", "_")+"="+s)
-			}
+// IsPathExecutable returns whether or not a given file is executable based on
+// its path and/or its permission bits (depending on the operating system).
+//
+// On Windows, a file is executable if and only if it has a file extension of
+// "exe" or "bat." On other operating systems, any file with the execute bit set
+// for at least one user is deemed executable.
+func IsPathExecutable(path string, fileinfo fs.FileInfo) (bool, error) {
+	switch runtime.GOOS {
+	case "windows":
+		// Register executable handlers based on file extension
+		switch filepath.Ext(path) {
+		case ".exe", ".bat":
+			return true, nil
 		}
 
-		// Pass request body on standard input
-		stdin, err := cmd.StdinPipe()
-		if err != nil {
-			logger.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
+	default:
+		// TODO: Does it make sense to look for files executable by any user?
+		filemode := fileinfo.Mode()
+		if !filemode.IsDir() && filemode.Perm()&0111 != 0 {
+			return true, nil
 		}
-		go func() {
-			defer stdin.Close()
-
-			if r.Method != "POST" || (len(r.Header["Content-Type"]) > 0 &&
-				r.Header["Content-Type"][0] == "application/x-www-form-urlencoded") {
-				// If the submission is a non-POST request, or is a form
-				// submission according to content type, treat it like a form
-				err := r.ParseForm()
-				if err != nil {
-					logger.Println(err)
-					http.Error(w, http.StatusText(500), 500)
-					return
-				}
-
-				form_data, err := DecodeForm(r.Form)
-				if err != nil {
-					logger.Println(err)
-					http.Error(w, http.StatusText(500), 500)
-					return
-				}
-				_, err = io.Copy(stdin, bytes.NewReader(form_data))
-				if err != nil {
-					logger.Println(err)
-					http.Error(w, http.StatusText(500), 500)
-					return
-				}
-			} else {
-				// This POST data is not form data (may be a JSON API request,
-				// for example), so don't encode it as a form. If it is a
-				// multipart or other form submission, it will be properly
-				// encoded already.
-				_, err := io.Copy(stdin, r.Body)
-				if err != nil {
-					logger.Println(err)
-					http.Error(w, http.StatusText(500), 500)
-					return
-				}
-			}
-		}()
-
-		// Print out stderror messages for debugging
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			logger.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		go func() {
-			defer stderr.Close()
-			data, _ := io.ReadAll(stderr)
-			if len(data) > 0 {
-				logger.Println(string(data))
-			}
-		}()
-
-		// Execute the command and write the output as the HTTP response
-		out, err := cmd.Output()
-		if err != nil {
-			logger.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-
-		w.Write(out)
 	}
+
+	return false, nil
 }
 
-// RegisterExecutableHandler associates the given route with a handler to
-// execute the file at the given path when accessed. It also modifies the
-// global list of routes.
-func RegisterExecutableHandler(mux *http.ServeMux, path, route, dir, filename string) {
-	handler := NewExecutableHandler(path)
+// ExecutePath runs an executable path that when accessed: executes the file at
+// the path, passes the request body via standard input, gets the response via
+// standard output and returns that as the response body.
+func ExecutePath(path string, w http.ResponseWriter, r *http.Request) {
+	logger.Println("Executing:", path)
 
-	mux.HandleFunc(route, handler)
-	routes[route] = ""
-
-	dirRoute := "/" + filepath.ToSlash(dir)
-	if _, in := routes[dirRoute]; strings.TrimSuffix(filename, filepath.Ext(filename)) == "index" && !in {
-		// Register handlers with and without the trailing "/" to avoid redirect
-		mux.HandleFunc(dirRoute, handler)
-		mux.HandleFunc(dirRoute+"/", handler)
-		routes[dirRoute+"/"] = route
+	if strings.HasPrefix(path, "/") {
+		path = "." + path
 	}
+	abspath, err := filepath.Abs(path)
+	if err != nil {
+		logger.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	dir, _ := filepath.Split(abspath)
+
+	// Create the command using all environment variables. Include a
+	// REQUEST_METHOD environment variable in imitation of CGI
+	cmd := exec.Command(abspath)
+	cmd.Env = append(os.Environ(), "REQUEST_METHOD="+r.Method)
+
+	// Execute the route in its own directory so relative paths in the executed
+	// program behave sensibly
+	cmd.Dir = dir
+
+	// Pass headers as environment variables in imitation of CGI
+	for k, v := range r.Header {
+		// The same header can have multiple values
+		for _, s := range v {
+			cmd.Env = append(cmd.Env, "HTTP_"+strings.ReplaceAll(k, "-", "_")+"="+s)
+		}
+	}
+
+	// Pass request body on standard input
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		logger.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	go func() {
+		defer stdin.Close()
+
+		if r.Method != "POST" || (len(r.Header["Content-Type"]) > 0 &&
+			r.Header["Content-Type"][0] == "application/x-www-form-urlencoded") {
+			// If the submission is a non-POST request, or is a form
+			// submission according to content type, treat it like a form
+			err := r.ParseForm()
+			if err != nil {
+				logger.Println(err)
+				http.Error(w, http.StatusText(500), 500)
+				return
+			}
+
+			form_data, err := DecodeForm(r.Form)
+			if err != nil {
+				logger.Println(err)
+				http.Error(w, http.StatusText(500), 500)
+				return
+			}
+			_, err = io.Copy(stdin, bytes.NewReader(form_data))
+			if err != nil {
+				logger.Println(err)
+				http.Error(w, http.StatusText(500), 500)
+				return
+			}
+		} else {
+			// This POST data is not form data (may be a JSON API request,
+			// for example), so don't encode it as a form. If it is a
+			// multipart or other form submission, it will be properly
+			// encoded already.
+			_, err := io.Copy(stdin, r.Body)
+			if err != nil {
+				logger.Println(err)
+				http.Error(w, http.StatusText(500), 500)
+				return
+			}
+		}
+	}()
+
+	// Print out stderror messages for debugging
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		logger.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	go func() {
+		defer stderr.Close()
+		data, _ := io.ReadAll(stderr)
+		if len(data) > 0 {
+			logger.Println(string(data))
+		}
+	}()
+
+	// Execute the command and write the output as the HTTP response
+	out, err := cmd.Output()
+	if err != nil {
+		logger.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	w.Write(out)
 }
 
-// RegisterPaths walks the current directory and registers handlers to run any
-// executable files it finds. The handlers are added as routes to the given
-// mux.
-func RegisterPaths(mux *http.ServeMux, logfileName string) error {
-	return filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
+// FindIndexFile returns the path to the index file of the directory path given
+// as input (if one exists). If there is no index file, or if there was a fatal
+// error during the search, the second returned value is false.
+func FindIndexFile(dir string) (string, bool) {
+	file, err := http.Dir(".").Open(dir)
+	if err != nil {
+		return "", false
+	}
+	defer file.Close()
+
+	files, err := file.Readdir(-1)
+	if err != nil {
+		return "", false
+	}
+	for _, file := range files {
+		filename := file.Name()
+		isExecutable, err := IsPathExecutable(filename, file)
+		if err != nil {
+			continue
+		}
+		if isExecutable && strings.TrimSuffix(filename, path.Ext(filename)) == "index" {
+			return path.Join(dir, filename), true
+		}
+	}
+	return "", false
+}
+
+// FindExecutablePaths walks the current directory and locates paths that will
+// be executed when visited. It returns them as a map.
+func FindExecutablePaths(logfileName string) (map[string]string, error) {
+	routes := make(map[string]string)
+
+	err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// Don't re-register any existing routes
+		// Clean up the path and format it HTTP-style
 		path = filepath.Clean(path)
-		route := "/" + filepath.ToSlash(path)
-		if _, in := routes[route]; in {
-			return nil
-		}
+		path = "/" + filepath.ToSlash(path)
 
-		// Ignore the executable for quickserv itself if it's in the directory
-		dir, filename := filepath.Split(path)
-		dir = filepath.Clean(dir)
+		// Ignore the executable for quickserv itself if it's in the directory.
+		// Also ignore the logfile if it's present.
+		_, filename := filepath.Split(path)
+		fileinfo, err := d.Info()
+		if err != nil {
+			return err
+		}
 		switch filename {
 		case "quickserv", "quickserv.exe", logfileName:
 			return nil
 		}
 
-		// Executables are represented differently on different operating systems
-		switch runtime.GOOS {
-		case "windows":
-			// Register executable handlers based on file extension
-			switch filepath.Ext(path) {
-			case ".exe", ".bat":
-				RegisterExecutableHandler(mux, path, route, dir, filename)
+		// Find the index file if path is a directory
+		if fileinfo.IsDir() {
+			index, found := FindIndexFile(path)
+			if found {
+				routes[path] = index
 			}
 
-		default:
-			// Check the permission bits, assume consistency across Linux/OS X/others
-			fileinfo, err := d.Info()
-			if err != nil {
-				return err
-			}
-			// TODO: Does it make sense to look for files executable by any user?
-			filemode := fileinfo.Mode()
-			if !filemode.IsDir() && filemode.Perm()&0111 != 0 {
-				RegisterExecutableHandler(mux, path, route, dir, filename)
+			return nil
+		}
+
+		// Print a result if executable
+		isExecutable, err := IsPathExecutable(path, fileinfo)
+		if err != nil {
+			return err
+		}
+		if isExecutable {
+			routes[path] = ""
+		}
+		return nil
+	})
+
+	return routes, err
+}
+
+// NewMainHandler returns an http.Handler that looks at the file a user requests
+// and decides whether to execute it, or pass it to an http.FileServer.
+func NewMainHandler(filesystem http.FileSystem) http.Handler {
+	fileserver := http.FileServer(filesystem)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Clean up the request path
+		reqPath := r.URL.Path
+		if !strings.HasPrefix(reqPath, "/") {
+			reqPath = "/" + reqPath
+			r.URL.Path = reqPath
+		}
+		reqPath = path.Clean(reqPath)
+
+		// Open the path in the filesystem for further inspection
+		f, err := filesystem.Open(reqPath)
+		if err != nil {
+			// If we can't open the file, let the FileServer handle it correctly
+			logger.Println(err)
+			fileserver.ServeHTTP(w, r)
+			return
+		}
+		defer f.Close()
+		d, err := f.Stat()
+		if err != nil {
+			logger.Println(err)
+			fileserver.ServeHTTP(w, r)
+			return
+		}
+
+		// If the path is a directory, look for an index file. If none found,
+		// serve up the directory. Otherwise, act like the executable was the
+		// requested path.
+		if d.IsDir() {
+			index, found := FindIndexFile(reqPath)
+			if !found {
+				fileserver.ServeHTTP(w, r)
+				return
+			} else {
+				reqPath = index
 			}
 		}
 
-		return nil
+		isExecutable, err := IsPathExecutable(reqPath, d)
+		if err != nil {
+			logger.Println(err)
+			fileserver.ServeHTTP(w, r)
+			return
+		}
+		if isExecutable {
+			// If the path is executable, run it
+			ExecutePath(reqPath, w, r)
+		} else {
+			fileserver.ServeHTTP(w, r)
+		}
 	})
 }
 
@@ -270,12 +362,9 @@ func RegisterPaths(mux *http.ServeMux, logfileName string) error {
 
 func main() {
 	// Parse command line arguments
-	// TODO: Handle long and short options
 	var logfileName, wd string
 	var randomPort bool
-	// flag.StringVar(&logfileName, "l", "-", "Log file path. Stdout if unspecified.")
 	flag.StringVar(&logfileName, "logfile", "-", "Log file path. Stdout if unspecified.")
-	// flag.StringVar(&wd, "d", ".", "Folder to serve files from.")
 	flag.StringVar(&wd, "dir", ".", "Folder to serve files from.")
 	flag.BoolVar(&randomPort, "random-port", false, "Use a random port instead of 42069.")
 	flag.Parse()
@@ -291,6 +380,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer logfile.Close()
 		if abspath, err := filepath.Abs(logfileName); err == nil {
 			fmt.Printf("Logging to folder:\n%v\n", abspath)
 		} else {
@@ -309,18 +399,11 @@ func main() {
 	}
 	fmt.Printf("Running in folder:\n%v\n\n", wd)
 
-	mux := http.NewServeMux()
-
-	// Walk the working directory looking for executable files and register
-	// handlers to execute them
-	routes = make(map[string]string)
-	err = RegisterPaths(mux, logfileName)
+	// Print non-static routes that will be executed (if any)
+	routes, err := FindExecutablePaths(logfileName)
 	if err != nil {
-		logger.Println("Failed while trying to find executables in the working directory!")
 		logger.Fatal(err)
 	}
-
-	// Print non-static routes that will be executed (if any)
 	if len(routes) > 0 {
 		fmt.Println("Files that will be executed if accessed: ")
 		for k, v := range routes {
@@ -338,9 +421,6 @@ func main() {
 		// fmt.Println("For more information see the documentation here: TODO")
 	}
 	fmt.Println("")
-
-	// Statically serve non-executable files that don't already have a handler
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	// Pick a random port if the user wants -- for slightly more professional
 	// demos where the number 42069 might be undesirable
@@ -364,5 +444,9 @@ func main() {
 	fmt.Println("Press Control + C to stop the server.")
 	fmt.Println()
 
-	logger.Fatal(http.ListenAndServe(":"+strconv.FormatInt(port, 10), mux))
+	// Build a handler that decides whether to serve static files or dynamically
+	// execute them
+	handler := NewMainHandler(http.Dir("."))
+	addr := ":" + strconv.FormatInt(port, 10)
+	logger.Fatal(http.ListenAndServe(addr, handler))
 }
